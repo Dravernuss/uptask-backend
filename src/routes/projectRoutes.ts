6e75d7f33ca95eb18c28175a7ba8da74ad94d9b3
Dravernuss <esteban16.rodas@gmail.com>
2025-02-20 21:19:36 -0500

    Creating CRUD for Projects

---
+++ src/routes/projectRoutes.ts
@@ -0,0 +1,55 @@
+import { Router } from "express";
+import { body, param } from "express-validator";
+import { ProjectController } from "../controllers/ProjectController";
+import { handleInputErrors } from "../middleware/validation";
+
+const router = Router();
+
+router.post(
+  "/",
+  body("projectName")
+    .notEmpty()
+    .withMessage("El Nombre del Proyecto es Obligatorio"),
+  body("clientName")
+    .notEmpty()
+    .withMessage("El Nombre del Cliente es Obligatorio"),
+  body("description")
+    .notEmpty()
+    .withMessage("La Descripción del Proyecto es Obligatorio"),
+  handleInputErrors,
+  ProjectController.createProject
+);
+
+router.get("/", ProjectController.getAllProjects);
+
+router.get(
+  "/:id",
+  param("id").isMongoId().withMessage("ID no válido"),
+  handleInputErrors,
+  ProjectController.getProjectById
+);
+
+router.put(
+  "/:id",
+  param("id").isMongoId().withMessage("ID no válido"),
+  body("projectName")
+    .notEmpty()
+    .withMessage("El Nombre del Proyecto es Obligatorio"),
+  body("clientName")
+    .notEmpty()
+    .withMessage("El Nombre del Cliente es Obligatorio"),
+  body("description")
+    .notEmpty()
+    .withMessage("La Descripción del Proyecto es Obligatorio"),
+  handleInputErrors,
+  ProjectController.updateProjectById
+);
+
+router.delete(
+  "/:id",
+  param("id").isMongoId().withMessage("ID no válido"),
+  handleInputErrors,
+  ProjectController.deleteProject
+);
+
+export default router;
