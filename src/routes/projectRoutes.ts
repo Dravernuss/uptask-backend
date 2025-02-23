f855f8852acfaf4ff362d6718848617386b0a1c7
Dravernuss <esteban16.rodas@gmail.com>
2025-02-22 23:33:03 -0500

    tasks api connect to project

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -2,6 +2,9 @@ import { Router } from "express";
 import { body, param } from "express-validator";
 import { ProjectController } from "../controllers/ProjectController";
 import { handleInputErrors } from "../middleware/validation";
+import { TaskController } from "../controllers/TaskController";
+import { projectExists } from "../middleware/project";
+import { taskBelongsToProject, taskExists } from "../middleware/task";
 
 const router = Router();
 
@@ -52,4 +55,53 @@ router.delete(
   ProjectController.deleteProject
 );
 
+/* Routes for tasks */
+router.param("projectId", projectExists);
+
+router.post(
+  "/:projectId/tasks",
+  body("name").notEmpty().withMessage("El Nombre de la tarea es Obligatorio"),
+  body("description")
+    .notEmpty()
+    .withMessage("La descripción de la tarea es obligatoria"),
+  handleInputErrors,
+  TaskController.createTask
+);
+
+router.get("/:projectId/tasks", TaskController.getProjectTasks);
+
+router.param("taskId", taskExists);
+router.param("taskId", taskBelongsToProject);
+router.get(
+  "/:projectId/tasks/:taskId",
+  param("taskId").isMongoId().withMessage("ID no válido"),
+  handleInputErrors,
+  TaskController.getTaskById
+);
+
+router.put(
+  "/:projectId/tasks/:taskId",
+  param("taskId").isMongoId().withMessage("ID no válido"),
+  body("name").notEmpty().withMessage("El Nombre de la tarea es Obligatorio"),
+  body("description")
+    .notEmpty()
+    .withMessage("La descripción de la tarea es obligatoria"),
+  handleInputErrors,
+  TaskController.updateTask
+);
+
+router.delete(
+  "/:projectId/tasks/:taskId",
+  param("taskId").isMongoId().withMessage("ID no válido"),
+  handleInputErrors,
+  TaskController.deleteTask
+);
+
+router.post(
+  "/:projectId/tasks/:taskId/status",
+  param("taskId").isMongoId().withMessage("ID no válido"),
+  body("status").notEmpty().withMessage("El estado es obligatorio"),
+  handleInputErrors,
+  TaskController.updateStatus
+);
 export default router;
