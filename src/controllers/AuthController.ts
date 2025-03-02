4bc3b9ecae0cb2cf05447317ed0f57a7ea4a0f58
Dravernuss <esteban16.rodas@gmail.com>
2025-03-02 14:00:22 -0500

    Adding authentication api

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -93,4 +93,36 @@ export class AuthController {
       res.status(500).json({ error: "Hubo un error" });
     }
   };
+
+  static requestConfirmationCode = async (req: Request, res: Response) => {
+    try {
+      const { email } = req.body;
+
+      const user = await User.findOne({ email });
+      if (!user) {
+        const error = new Error("El Usuario no esta registrado");
+        return res.status(404).json({ error: error.message });
+      }
+
+      if (user.confirmed) {
+        const error = new Error("El Usuario ya esta confirmado");
+        return res.status(403).json({ error: error.message });
+      }
+
+      const token = new Token();
+      token.token = generateToken();
+      token.user = user.id;
+
+      AuthEmail.sendConfirmationEmail({
+        email: user.email,
+        name: user.name,
+        token: token.token,
+      });
+
+      await Promise.allSettled([user.save(), token.save()]);
+      res.send("Se envió un nuevo token a tu email");
+    } catch (error) {
+      res.status(500).json({ error: "Hubo un error" });
+    }
+  };
 }
